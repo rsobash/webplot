d98f65dcd97b8bc91fa3b5501117af6379d89973
ahijevyc <ahijevyc@ucar.edu>
2023-02-10 12:59:44 -0700

    Clean version of ~ahijevyc/lib/python3.6/mpas.py
    no reference to atcf

--- mpas.py
+++ mpas.py
@@ -1,80 +1,10 @@
-import atcf
 import datetime
 from fieldinfo import fieldinfo,readNCLcm
 import logging
-from netCDF4 import Dataset
 import numpy as np
 import os
-import pandas as pd
-import pdb
 import re
 import sys
-import xarray
-
-def get_diag_name(valid_time, prefix='diag.', suffix='.nc'):
-    diag_name = prefix + valid_time.strftime("%Y-%m-%d_%H.%M.%S") + suffix
-    return diag_name
-
-def raw_vitals(row, diagdir, lonCell, latCell, wind_radii_method=None):
-    row = row.head(1).squeeze() # make multiple rad lines one series
-    assert 'originalmeshfile' not in row, f"{row} already has original mesh vitals"
-    diagfile = get_diag_name(row.valid_time, prefix='diag.', suffix='.nc')
-
-    diagfile = os.path.join(diagdir,diagfile)
-    logging.debug(f"reading diagfile {diagfile}")
-    ds = xarray.open_dataset(diagfile)
-    ds = ds[['u10','v10','mslp']].metpy.quantify() # Don't choke on 'dBZ' not defined in unit registry
-    ds = ds.isel(Time=0)
-
-    # Extract vmax, RMW, minp, and radii of wind thresholds
-    derived_winds_dict = atcf.derived_winds(ds.u10, ds.v10, ds.mslp, lonCell, latCell, row, wind_radii_method=wind_radii_method)
-    row = atcf.unitless_row(derived_winds_dict, row)
-
-    row["originalmeshfile"] = diagfile
-    # replace the row with (possibly) multiple rows with different wind radii (34/50/64 knot) 
-    row = atcf.add_wind_rad_lines(row, derived_winds_dict["wind_radii"])
-    return row
-
-
-def origmesh(df, initfile, diagdir, wind_radii_method="max"):
-    # assert this is a single track
-    assert df.groupby(['basin','cy','initial_time','model']).ngroups == 1, 'mpas.origmesh got more than 1 track'
-
-    # Get raw values from MPAS mesh for one track
-
-    # input 
-    # df = pandas Dataframe version of atcf data
-    # init.nc = path to file with mesh cells lat/lon (first time this is run)
-    #           or a dictionary containing mesh cells lat/lon (faster)
-    # diagdir = path to directory with diagnostics files.
-
-
-    # The first time this is called initfile is a simple string.
-    # Next time, it is a dictionary with all the needed variables.
-    if isinstance(initfile, str):
-        logging.debug(f"reading lat/lon from {initfile}")
-        init = xarray.open_dataset(initfile)
-        lonCell = init['lonCell'].metpy.convert_units("degrees")
-        latCell = init['latCell'].metpy.convert_units("degrees")
-        lonCell[lonCell >= 180] = lonCell[lonCell >=180] - 360.
-        init.close()
-        initfile = {
-                "initfile":initfile,
-                "lonCell":lonCell,
-                "latCell":latCell,
-                }
-    else:
-        logging.debug("reading lat/lon from dictionary")
-        lonCell = initfile["lonCell"]
-        latCell = initfile["latCell"]
-
-    # Only groupby 'fhr'. This should already be a single unique track.
-    df = df.groupby('fhr').apply(raw_vitals,diagdir,lonCell,latCell,wind_radii_method=wind_radii_method)
-    if "fhr" in df.index.names: # Don't know why it sometimes is not an index
-        # don't return with fhr as index
-        df = df.droplevel('fhr')
-    return df, initfile
-
 
 # fieldinfo should have been imported from fieldinfo module.
 # Copy fieldinfo dictionary for MPAS. Change some fnames and filenames.
@@ -174,27 +104,3 @@ def makeEnsembleListMPAS(wrfinit, timerange, ENS_SIZE, g193=False):
     if not file_list['diag']:
         logging.info('Empty file_list')
     return (file_list, missing_list)
-
-# From NCAR geocat example
-
-# This funtion splits a global mesh along longitude
-#
-# Examine the X coordinates of each triangle in 'tris'. Return an array of 'tris' where only those triangles
-# with legs whose length is less than 't' are returned. 
-# 
-def unzipMesh(x,tris,t):
-    return tris[(np.abs((x[tris[:,0]])-(x[tris[:,1]])) < t) & (np.abs((x[tris[:,0]])-(x[tris[:,2]])) < t)]
-
-# Compute the signed area of a triangle
-#
-def triArea(x,y,tris):
-    return ((x[tris[:,1]]-x[tris[:,0]]) * (y[tris[:,2]]-y[tris[:,0]])) - ((x[tris[:,2]]-x[tris[:,0]]) * (y[tris[:,1]]-y[tris[:,0]]))
-
-# Reorder triangles as necessary so they all have counter clockwise winding order. CCW is what Datashader and MPL
-# require.
-#
-def orderCCW(x,y,tris):
-    tris[triArea(x,y,tris)<0.0,:] = tris[triArea(x,y,tris)<0.0,::-1]
-    return(tris)
-
-
