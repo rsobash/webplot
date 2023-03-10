b66460e2b5de94ae47280c30485d0e9c70b96e55
ahijevyc <ahijevyc@ucar.edu>
2023-03-10 15:23:00 -0700

    simple defaultdict fieldinfo. change stp['fname']

--- fieldinfo.py
+++ fieldinfo.py
@@ -28,8 +28,7 @@ def readNCLcm(name):
     return rgb.tolist()
 
 
-dd = dict(levels=range(10), cmap=readNCLcm('prcp_1'), fname=[], filename='diag')
-fieldinfo = defaultdict(lambda:dd)
+fieldinfo = defaultdict(dict)
 
 fieldinfo.update({
   # surface and convection-related entries
@@ -130,7 +129,7 @@ fieldinfo.update({
                   'fname': ['PVORT_320K'], 'filename':'upp' },
  'speed10m'     :{ 'levels' : [3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51], 'cmap': readNCLcm('wind_17lev')[1:],'fname'  : ['u10', 'v10'], 'filename':'diag'},
  'speed10m-tc'  :{ 'levels' : [6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102], 'cmap': readNCLcm('wind_17lev')[1:],'fname'  : ['u10', 'v10'], 'filename':'diag'},
- 'stp'          :{ 'levels' : [0.5,0.75,1.0,1.5,2.0,3.0,4.0,5.0,6.0,7.0,8.0], 'cmap':readNCLcm('perc2_9lev'), 'fname':['sbcape','lcl','srh_0_1km','uzonal_6km','umeridional_6km','uzonal_surface','umeridional_surface'], 'filename':'diag'},
+ 'stp'          :{ 'levels' : [0.5,0.75,1.0,1.5,2.0,3.0,4.0,5.0,6.0,7.0,8.0], 'cmap':readNCLcm('perc2_9lev'), 'fname':['CAPE_221_SFC','CIN_221_SFC','HLCY_221_HTGY'] },
  'uhratio'      :{ 'levels' : [0.1,0.3,0.5,0.7,0.9,1.0,1.1,1.2,1.3,1.4,1.5], 'cmap':readNCLcm('perc2_9lev'), 'fname':['updraft_helicity_max03', 'updraft_helicity_max'], 'filename':'diag'},
  'crefuh'       :{ 'levels' : [5,10,15,20,25,30,35,40,45,50,55,60,65,70], 'cmap': readcm('/glade/u/home/sobash/RT2015_gpx/cmap_rad.rgb')[0:13], 'fname': ['refl10cm_max', 'updraft_helicity_max'], 'filename':'diag' },
 
